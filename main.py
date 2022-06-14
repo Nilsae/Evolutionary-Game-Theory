f6c5e1a542585c1c16f2e86b2282aaa5b3a44002
nilsa <n.saeidi174@gmail.com>
2022-06-14 12:57:58 +0430

    code works right, algorithm does not!

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pandas import concat
 
 
 def main():
-    population = 9 # number of agents
+    population = 8 # number of agents
     num_episode = 1    # Number of total episode in a single simulation for taking ensemble average
     time_steps =30
     threshold = 1/2
@@ -18,9 +18,16 @@ def main():
     always_equilibrates = 1
     global new_result
     result = pd.DataFrame({'reached_desired?': [],'Equilibrated?': [],'types': [],'initial strategy set': [],'final strategy set': [],'equilibration time': []})
-    desired_eq = ['A', 'A', 'B', 'A', 'B', 'A', 'A', 'B', 'A'] #desired equilibrium strategy of the agents
-    type_list = ['+', '+', '-', '-', '-', '+', '-', '-', '-'] #coordinator or anticoordinator listed identified by + and - signs
-    initial_strategy  =['B', 'A', 'B', 'A', 'A', 'A', 'A', 'A', 'A'] #initial strategy of the agents
+    # algorithm does not work on(pop=9): (loops :))
+    # desired_eq = ['A', 'A', 'B', 'A', 'B', 'A', 'A', 'B', 'A'] #desired equilibrium strategy of the agents
+    # type_list = ['+', '+', '-', '-', '-', '+', '-', '-', '-'] #coordinator or anticoordinator listed identified by + and - signs
+    # initial_strategy  =['B', 'A', 'B', 'A', 'A', 'A', 'A', 'A', 'A'] #initial strategy of the agents
+    #it works on: (original example ;))
+    desired_eq = ['A', 'B', 'A', 'A', 'B', 'A', 'A', 'A']  # desired equilibrium strategy of the agents
+    type_list = ['+', '-', '-', '+', '-', '-', '+', '+']  # coordinator or anticoordinator listed identified by + and - signs
+    initial_strategy = ['B', 'A', 'B', 'A', 'A', 'B', 'A',
+                        'B']  # initial strategy of the agents# selection = [i for i in range(population)]
+
     # selection = [i for i in range(population)]
     activated_list = zeros(population)
     simulation = Simulation(population, Z_func, initial_strategy, type_list,activated_list, desired_eq)
